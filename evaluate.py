b6379d65b63a48a1841455068a67084f38a7ce54
Mohamed Abu El-Nasr <64566340+abuelnasr0@users.noreply.github.com>
2024-10-04 00:53:18 +0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -47,7 +47,7 @@ def main():
         from util_compute import predict_classification_mt0_by_letter as predict_classification
     else:
         print(model_class)
-        model = model_class.from_pretrained(args.base_model, load_in_8bit=args.load_8bit, trust_remote_code=True, torch_dtype=torch.float16)
+        model = model_class.from_pretrained(args.base_model, load_in_8bit=args.load_8bit, trust_remote_code=True, torch_dtype=torch.bfloat16)
         from util_compute import predict_classification_causal_by_letter as predict_classification
     
     # Load adapter if we use adapter
